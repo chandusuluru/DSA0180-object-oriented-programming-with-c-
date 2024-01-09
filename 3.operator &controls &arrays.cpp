8fbbc06094528b59ab7f7093eeaf0a1f2364d5b9
chandusuluru <113323442+chandusuluru@users.noreply.github.com>
2024-01-09 14:48:08 +0530

    Add files via upload

---
+++ 3.operator &controls &arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    std::cout << "Enter the first integer: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second integer: ";
+    std::cin >> num2;
+    if (num1 == num2) {
+        std::cout << "The two integers are equal." << std::endl;
+    } else {
+        std::cout << "The two integers are not equal." << std::endl;
+    }
+
+    return 0;
+}
+
